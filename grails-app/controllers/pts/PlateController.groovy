40ba511578aad0ef0b34cb104842648a766cb9e7
imnderoo <nderoo.wong@gmail.com>
2013-09-27 16:59:21 -0400

    Finished function to create 96-well Plates from Manifest
    1. Save Investigator if doesn't exist
    2. Save Project if doesn't exist
    3. Read Plate information and create new Plate based on IntSampleID
    4. Read Sample information and create new entry for each Sample in plate

--- grails-app/controllers/pts/PlateController.groovy
+++ grails-app/controllers/pts/PlateController.groovy
@@ -1,244 +1,265 @@
 package pts
 
-import java.sql.RowId;
-
-import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.Row
-import org.apache.poi.ss.usermodel.Sheet
-import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.springframework.dao.DataIntegrityViolationException
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartFile
 
 class PlateController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    def index() {
-        redirect(action: "list", params: params)
-    }
+	def index() {
+		redirect(action: "list", params: params)
+	}
 
-    def list(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        [plateInstanceList: Plate.list(params), plateInstanceTotal: Plate.count()]
-    }
+	def list(Integer max) {
+		params.max = Math.min(max ?: 10, 100)
+		[plateInstanceList: Plate.list(params), plateInstanceTotal: Plate.count()]
+	}
 
-    def create() {
+	def create() {
 		println("CREATE")
 		println(params)
-        [plateInstance: new Plate(params)]
-    }
+		[plateInstance: new Plate(params)]
+	}
 
-    def save() {
-		
-		def plateInstance = new Plate(params)
-		
-//		if (!plateInstance.save(flush: true)) {
-//			render(view: "create", model: [plateInstance: plateInstance])
-//			return
-//		}
-			
+	def save() {
+		println("SAVE")
+		println(params)
+
+		//def plateInstance = new Plate(params)
+
 		// Fetch Plate Manifest And Create Excel Object
-		
+
 		def file = request.getFile("file")
-//		def fileName = file.getOriginalFilename()
-//		println(fileName)
-		
+
 		def wb = WorkbookFactory.create(file.getInputStream())
 		def sheet = wb.getSheetAt(0)
 		def row
 		def cell
-		
+
 		def plateMap = [:]
 		def projectMap = [:]
 		def investigatorMap = [:]
 
 		def plateTypeInstance = PlateType.findByName('Plate96')
-		 
-		// Read investigator
+
+		// Read Investigator Info
 		row = sheet.getRow(0)
 		cell = row.getCell(1)
-		String cellVal = cell.getStringCellValue()
-		println(cellVal)
-		investigatorMap['firstName'] = "John"
-		investigatorMap['lastName'] = "Smith"
-		
-		// Save Investigator
-		def investigatorInstance = new Investigator(investigatorMap)
-		
-		println(investigatorMap)
-		
-//		investigatorInstance.save(flush: true)
-		// Add Investigator to Project Map
-//		projectMap['investigator'] = Investigator.findByFirstName(investigatorMap['firstName'])
-		
-		// Read project name
+		def investigatorName = cell.getStringCellValue()
+		def firstName = investigatorName.split(' ')[0..-2].join(' ')
+		def lastName = investigatorName.split(' ')[-1]
+		// Check if investigator exists...
+		def investigatorInstance = Investigator.findByFirstNameAndLastName(firstName, lastName)
+		// If not, create new investigator
+		if (investigatorInstance == null)
+		{
+			investigatorMap['firstName'] = firstName
+			investigatorMap['lastName'] = lastName
+			investigatorInstance = new Investigator(investigatorMap)
+			if(!investigatorInstance.save(flush:true))
+			{
+				investigatorInstance.errors.each {
+					println it
+				}
+			}
+		}
+
+		// Read Project Info
 		row = sheet.getRow(2)
 		cell = row.getCell(1, Row.CREATE_NULL_AS_BLANK)
-		
-		projectMap['name'] = cell.getStringCellValue()
-//		projectMap['investigator'] = investigatorInstance
-		
-		
-		// Save Project
-		def projectInstance = new Project(projectMap)
-		
-//		investigatorInstance.addToProject(projectInstance)
-		
-		if (!investigatorInstance.save(flush: true)) {
-			render(view: "create", model: [plateInstance: plateInstance])
-			println("NOT SAVED")
-			return
-		}		
-		else
+		def projectName = cell.getStringCellValue()
+		// Check if project exists...
+		def projectInstance = Project.findByInvestigatorAndName(investigatorInstance, projectName)
+		// If not, create new Project
+		if (projectInstance == null)
 		{
-			println("SAVED")
+			projectMap['name'] = projectName
+			projectMap['investigator'] = investigatorInstance
+			projectInstance = new Project(projectMap)
+			if (!projectInstance.save(flush:true))
+			{
+				projectInstance.errors.each {
+					println it
+				}
+			}
 		}
-//		projectInstance.save(flush: true)
+
+		// Read Plate Info
+
+		// ExtPlateId
+		row = sheet.getRow(6)
+		cell = row.getCell(0, Row.CREATE_NULL_AS_BLANK)
+		def extPlateId = cell.getStringCellValue()
+
+		// CreatedDate
+		row = sheet.getRow(3)
+		cell = row.getCell(1, Row.CREATE_NULL_AS_BLANK)
+		def createdDateString = cell.getStringCellValue()
+		def createdDate = new Date()
+		// If field is not empty, update date.
+		if (!createdDateString.isEmpty())
+		{
+			createdDate = Date.parse("dd-MM-yyyy", createdDateString)
+		}
+
+
+		def plateInstance = Plate.findByIntPlateId(params.intPlateId.toUpperCase())
+
+		if (plateInstance == null)
+		{
+			plateMap['intPlateId'] = params.intPlateId.toUpperCase()
+			plateMap['extPlateId'] = extPlateId
+			plateMap['plateType'] = PlateType.findByName("Plate96")
+			plateMap['createdDate'] = createdDate
+			plateMap['project'] = projectInstance
+			//TODO: Replace createdBy with logged in user name
+			plateMap['createdBy'] = investigatorInstance.getFirstName()
+			plateInstance = new Plate(plateMap)
+
+			if (!plateInstance.save(flush:true))
+			{
+				plateInstance.errors.each {
+					println it
+				}
+			}
+		}
+
+		def sampleList = []
+
+		for (rowNo in 7..101)
+		{
+			def samplesInPlateMap = [:]
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(1)
+			samplesInPlateMap['well'] = cell.getStringCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(2, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['sampleId'] = cell.getStringCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(3, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['sampleVol'] = cell.getNumericCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(4, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['dnaAmount'] = cell.getNumericCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(5, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['dnaSource'] = cell.getStringCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(6, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['dnaType'] = cell.getStringCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(7, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['dnaExtract'] = cell.getStringCellValue()
+
+			row = sheet.getRow(rowNo)
+			cell = row.getCell(8, Row.CREATE_NULL_AS_BLANK)
+			samplesInPlateMap['comment'] = cell.getStringCellValue()
+
+			samplesInPlateMap['plate'] = plateInstance
+
+			def sampleInstance = new SamplesInPlate(samplesInPlateMap)
+
+			// Queue samples
+			sampleList.add(sampleInstance)
+		}
+
+		for (samples in sampleList)
+		{
+			if (!samples.save())
+			{
+				samples.errors.each {
+					println it
+				}
+			}
+		}
 		
-		// Add Project to Plate Map
-//		plateMap['project'] = Project.findByName(projectMap['name'])
-			
-		render(view: "create", model: [plateInstance: plateInstance])
+		plateInstance.refresh()
+
+		render(view: "show", model: [plateInstance: plateInstance])
 		return
-		
-//		// Read created date
-//		row = sheet.getRow(3)
-//		cell = row.getCell(1, Row.CREATE_NULL_AS_BLANK)
-//		plateMap['createdDate'] = cell.getStringCellValue()
-//		
-//		// Read plate id
-//		row = sheet.getRow(6)
-//		cell = row.getCell(0, Row.CREATE_NULL_AS_BLANK)
-//		plateMap['intPlateId'] = cell.getStringCellValue()
-//		plateMap['extPlateId'] = cell.getStringCellValue()
-//		plateMap['plateType'] = plateTypeInstance()
-//		
-//		def sampleList = []
-//		
-//		for (rowNo in 6..101)
-//		{
-//			def sampleMap = [:]
-//			def sampleInPlateMap = [:]
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(1)
-//			sampleInPlateMap['well'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(2, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['sampleId'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(3, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['sampleVol'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(4, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['dnaAmount'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(5, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['dnaSource'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(6, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['dnaType'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(7, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['dnaExtract'] = cell.getStringCellValue()
-//			
-//			row = sheet.getRow(rowNo)
-//			cell = row.getCell(8, Row.CREATE_NULL_AS_BLANK)
-//			sampleMap['comment'] = cell.getStringCellValue()
-//			
-//			def sampleInstance = new Sample(sampleMap)
-//			
-//			sampleList.add(sampleInstance)
-//			
-//			def samplesInPlateInstance = new SampleInPlate()
-//			
-//		}
-//		
-//		for (samples in sampleList)
-//		{
-//			
-//		}
-//		
-//		
-//        flash.message = message(code: 'default.created.message', args: [message(code: 'plate.label', default: 'Plate'), plateInstance.id])
-//        redirect(action: "show", id: plateInstance.id)
-    }
 
-    def show(Long id) {
-        def plateInstance = Plate.get(id)
-        if (!plateInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
-            redirect(action: "list")
-            return
-        }
+		//
+		//
+		//        flash.message = message(code: 'default.created.message', args: [message(code: 'plate.label', default: 'Plate'), plateInstance.id])
+		//        redirect(action: "show", id: plateInstance.id)
+	}
 
-        [plateInstance: plateInstance]
-    }
+	def show(Long id) {
+		def plateInstance = Plate.get(id)
+		if (!plateInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
+			redirect(action: "list")
+			return
+		}
 
-    def edit(Long id) {
-        def plateInstance = Plate.get(id)
-        if (!plateInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
-            redirect(action: "list")
-            return
-        }
+		[plateInstance: plateInstance]
+	}
 
-        [plateInstance: plateInstance]
-    }
+	def edit(Long id) {
+		def plateInstance = Plate.get(id)
+		if (!plateInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
+			redirect(action: "list")
+			return
+		}
 
-    def update(Long id, Long version) {
-        def plateInstance = Plate.get(id)
-        if (!plateInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
-            redirect(action: "list")
-            return
-        }
+		[plateInstance: plateInstance]
+	}
 
-        if (version != null) {
-            if (plateInstance.version > version) {
-                plateInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'plate.label', default: 'Plate')] as Object[],
-                          "Another user has updated this Plate while you were editing")
-                render(view: "edit", model: [plateInstance: plateInstance])
-                return
-            }
-        }
+	def update(Long id, Long version) {
+		def plateInstance = Plate.get(id)
+		if (!plateInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
+			redirect(action: "list")
+			return
+		}
 
-        plateInstance.properties = params
+		if (version != null) {
+			if (plateInstance.version > version) {
+				plateInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						[message(code: 'plate.label', default: 'Plate')] as Object[],
+						"Another user has updated this Plate while you were editing")
+				render(view: "edit", model: [plateInstance: plateInstance])
+				return
+			}
+		}
 
-        if (!plateInstance.save(flush: true)) {
-            render(view: "edit", model: [plateInstance: plateInstance])
-            return
-        }
+		plateInstance.properties = params
 
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'plate.label', default: 'Plate'), plateInstance.id])
-        redirect(action: "show", id: plateInstance.id)
-    }
+		if (!plateInstance.save(flush: true)) {
+			render(view: "edit", model: [plateInstance: plateInstance])
+			return
+		}
 
-    def delete(Long id) {
-        def plateInstance = Plate.get(id)
-        if (!plateInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
-            redirect(action: "list")
-            return
-        }
+		flash.message = message(code: 'default.updated.message', args: [message(code: 'plate.label', default: 'Plate'), plateInstance.id])
+		redirect(action: "show", id: plateInstance.id)
+	}
 
-        try {
-            plateInstance.delete(flush: true)
-            flash.message = message(code: 'default.deleted.message', args: [message(code: 'plate.label', default: 'Plate'), id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'plate.label', default: 'Plate'), id])
-            redirect(action: "show", id: id)
-        }
-    }
+	def delete(Long id) {
+		def plateInstance = Plate.get(id)
+		if (!plateInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'plate.label', default: 'Plate'), id])
+			redirect(action: "list")
+			return
+		}
+
+		try {
+			plateInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [message(code: 'plate.label', default: 'Plate'), id])
+			redirect(action: "list")
+		}
+		catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'plate.label', default: 'Plate'), id])
+			redirect(action: "show", id: id)
+		}
+	}
 }
