43804897235640a0ea6806c2ec20fd7ddc2c6d81
imnderoo <nderoo.wong@gmail.com>
2013-10-30 14:04:48 -0400

    Autocomplete for searching 96plates

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -9,6 +9,37 @@ if (typeof jQuery !== 'undefined') {
 		$('.form-load').submit(function() {
 			$('#spinner').fadeIn();
 		});
-
 	})(jQuery);
 }
+
+$(function() {
+
+	$(".searchPlate96").autocomplete({
+		source : searchPlate96Path,
+		minLength : 4,
+		delay : 50,
+		autoFocus : true,
+		
+		open : function(event, ui) {
+			var field_id=$(this).attr('id').replace('.name', '\\.id');
+			$('#' + field_id).val('-1');
+		},
+
+		select : function(event, ui) {
+			var field_id=$(this).attr('id').replace('.name', '\\.id');
+			$('#' + field_id).val(ui.item.id);
+		},
+
+		close : function(event, ui) {
+			var field_name=$(this).attr('id');
+			var field_id=$(this).attr('id').replace('.name', '\\.id');
+			if ($('#' + field_id).val() == '-1') {
+				$('#' + field_name).val('');
+			}
+		}
+	});
+	
+	$(".datepicker").datepicker({
+		dateFormat: "dd-mm-yy"	
+	});
+});
