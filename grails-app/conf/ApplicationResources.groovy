d29426381bee42169bf6bd5bffbd764cb8ac66d9
imnderoo <nderoo.wong@gmail.com>
2013-10-30 14:02:58 -0400

    Add jqueryUI to resources

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -7,6 +7,7 @@ modules = {
 		resource url: [  dir: 'css', file: 'my.css' ], attrs:[ rel: "stylesheet", type:'css' ], bundle: 'bundle_app'
 
 		resource url:'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', bundle: 'bundle_app'
+		resource url:'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js', bundle: 'bundle_app'
 		resource url: [ dir: 'bootstrap-2.3.2/js', file: 'bootstrap.min.js'], bundle: 'bundle_app'
 		resource url: [ dir: 'jasny-bootstrap/js', file: 'bootstrap.min.js'], bundle: 'bundle_app'
 		resource url: [  dir: 'js', file: 'application.js' ], bundle: 'bundle_app'
