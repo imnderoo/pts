05a6d163d52db579045befc006dea43babe04e9e
imnderoo <nderoo.wong@gmail.com>
2014-01-29 17:48:27 -0500

    Updated DataSource credentials

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,52 +1,50 @@
 dataSource {
-    pooled = true
-    driverClassName = "com.mysql.jdbc.Driver"
+	pooled = true
+	driverClassName = "com.mysql.jdbc.Driver"
 
 
-     properties {
-        
-        maxActive = 10
-        maxIdle = 5
-        minIdle = 2
-        initialSize = 2
-        minEvictableIdleTimeMillis = 60000
-        timeBetweenEvictionRunsMillis = 60000
-        maxWait = 10000  
-        testWhileIdle = true
-        validationQuery = "select 1 "
-        
-    }
+	properties {
+
+		maxActive = 10
+		maxIdle = 5
+		minIdle = 2
+		initialSize = 2
+		minEvictableIdleTimeMillis = 60000
+		timeBetweenEvictionRunsMillis = 60000
+		maxWait = 10000
+		testWhileIdle = true
+		validationQuery = "select 1 "
+	}
 }
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
-    
-    dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
+	cache.use_second_level_cache=true
+	cache.use_query_cache=true
+	cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+
+	dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
 }
 // environment specific settings
 environments {
-    development {
-        dataSource {
-			  username = "root"
-			  url = "jdbc:mysql://localhost/ptrack"
-			  password = "1000anag3r"
-			  
-			  dbCreate = "create-drop"
-//			  dbCreate = "update"
-			             
-        }
-    }
-    production {
+	development {
+		dataSource {
+			username = "root"
+			password = "1000anag3r"
+
+			url = "jdbc:mysql://localhost/ptrack"
+
+			dbCreate = "update"
+		}
+	}
+	production {
 		dataSource {
 
 			username = "root"
 			password = "1000anag3r"
 
 			url = "jdbc:mysql://localhost/ptrack" // Uncomment when deploying
-			//url = "jdbc:mysql://localhost:3307/analysis"
+
 			dbCreate = "update"
-        }
-    }
-       
+		}
+	}
+
 }
